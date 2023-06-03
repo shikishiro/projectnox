36241d48441f7b30311dfe2ee02d8cbf84bd1c58
Shiro Shiki <65901788+shikishiro@users.noreply.github.com>
2023-06-03 08:05:29 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+DISCORD_TOKEN = ""
+OPENAI_API_KEY = ""
+OPENWEATHER_API_KEY = ""
+UNSPLASH_API_KEY = ""
+TRACKER_API_KEY = ""
+
+#Insert your API KEYS HERE 
+# Discord Token from Discord Developer
+# OPEN AI from ChatGPT Open AI
+# Open Weather for the Weather Monitoring
+# Unsplash for Image search
+# Tracker for Valorant Tracker
